0b091e6964997f0b0a638c30538a268109248b78
Jeremie Bidaux <jbidaux@c1r11s7.42lausanne.ch>
2024-01-16 16:51:12 +0100

    added path function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:35:24 by jbidaux           #+#    #+#             */
-/*   Updated: 2024/01/16 15:57:47 by jbidaux          ###   ########.fr       */
+/*   Updated: 2024/01/16 16:49:38 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,45 @@ int	path(t_data *data)
 {
 	const char	*path = getenv("PATH");
 	char	**split;
-	char	full_path;
+	char	*full_path;
+	char	*temp;
+	int	i;
+	int	j;
+	int	k;
 
+	i = 0;
+	k = 0;
 	split = ft_split(path, ':');
-	while (*split)
+	while (split[i])
 	{
-
+		j = 0;
+		while (j < data->cmd_y)
+		{
+			temp = ft_strjoin(split[i], "/");
+			full_path = ft_strjoin(temp, data->cmd[j].split[0]);
+			free(temp);
+			if (access(full_path, X_OK) == 0)
+			{
+				free(data->cmd[j].split[0]);
+				data->cmd[j].split[0] = full_path;
+				k++;
+			}
+			else
+				free(full_path);
+			j++;
+		}
+		i++;
 	}
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	if (k != data->cmd_y)
+		return (-1);
+	return (0);
 }
 //       /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin
 void	parsing(t_data *data, int ac, char **av)
@@ -83,17 +115,23 @@ int	ini(t_data *data, int ac, char **av)
 {
 	parsing(data, ac, av);
 	sep_arg(data);
+	if (path(data) < 0)
+	{
+		perror("wrong commands");
+		exit(0);
+	}
 	return (0);
 }
 
 int	main(int ac, char *av[]/* , char *envp[] */)
 {
 	t_data	data;
-	int		fds[2];
-	pid_t	pid;
+//	int		fds[2];
+//	pid_t	pid;
+	int	i;
 
 	ini(&data, ac, av);
-	pipe(fds);
+/* 	pipe(fds);
 	if (pipe(fds) == -1)
 	{
 		perror("pipe");
@@ -115,10 +153,10 @@ int	main(int ac, char *av[]/* , char *envp[] */)
 		close(data.file2);
 		if (execve("PATH", data.cmd[0].split, av[1]) < 0)
 			perror("Could not execve");
-	}
+	} */
 
-	close(fds[0])
-/* 	i = 0;
+//	close(fds[0])
+	i = 0;
 	while(i < ac - 3)
 	{
 		ft_printf("%s\n", data.cmd[i].cmd);
@@ -130,8 +168,8 @@ int	main(int ac, char *av[]/* , char *envp[] */)
 		SS(data.cmd[0].split[i]);
 		SS("\n");
 		i++;
-	} */
-	SS(getenv("PATH"));
+	}
+	// SS(getenv("PATH"));
 	clean(&data);
 	return (0);
 }
