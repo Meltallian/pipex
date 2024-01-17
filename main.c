518ce1096cf958dc2f26e0db395c967b28b74cb7
Jeremie Bidaux <jbidaux@c1r11s7.42lausanne.ch>
2024-01-17 14:57:37 +0100

    it works

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:35:24 by jbidaux           #+#    #+#             */
-/*   Updated: 2024/01/17 12:20:16 by jbidaux          ###   ########.fr       */
+/*   Updated: 2024/01/17 14:43:09 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ int	ini(t_data *data, int ac, char **av)
 	return (0);
 }
 
-int	main(int ac, char *av[]/* , char *envp[] */)
+int	main(int ac, char *av[], char *envp[])
 {
 	t_data	data;
-//	int		fds[2];
-//	pid_t	pid;
-	int	i;
+	int		fds[2];
+	pid_t	pid;
+	int		status;
+	pid_t	wpid;
 
 	ini(&data, ac, av);
-/* 	pipe(fds);
 	if (pipe(fds) == -1)
 	{
 		perror("pipe");
@@ -49,16 +49,32 @@ int	main(int ac, char *av[]/* , char *envp[] */)
 	{
 		dup2(data.file1, STDIN_FILENO);
 		close(data.file1);
-		dup2(fds[1], STDOUT_FILENO);
+		dup2(data.file2, STDOUT_FILENO);
+		close(data.file2);
 		close(fds[1]);
 		close(fds[0]);
 		close(data.file2);
-		if (execve("PATH", data.cmd[0].split, av[1]) < 0)
+		if (execve(data.cmd[0].split[0], data.cmd[0].split, envp) < 0)
+		{
 			perror("Could not execve");
-	} */
+			exit(EXIT_FAILURE);
+		}
+	}
+	close(fds[1]);
+	close(fds[0]);
+	close(data.file1);
+	close(data.file2);
+	wpid = waitpid(pid, &status, 0); //wait for the child to finish
+	clean(&data);
+	if (wpid == pid && WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else
+		return (-1);
+}
 
-//	close(fds[0])
-	i = 0;
+// PATH= /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin
+
+/* 	i = 0;
 	while(i < ac - 3)
 	{
 		ft_printf("%s\n", data.cmd[i].cmd);
@@ -84,11 +100,5 @@ int	main(int ac, char *av[]/* , char *envp[] */)
 		SS(data.cmd[2].split[i]);
 		SS("\n");
 		i++;
-	}
+	} */
 	// SS(getenv("PATH"));
-	clean(&data);
-	return (0);
-}
-
-// PATH= /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin
-//       /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin
